e19724422e31920430b82eb354ef3aa8f3b76fa3
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-11-04 18:50:19 +0100

    add comments

--- cin_cout.cpp
+++ cin_cout.cpp
@@ -4,12 +4,13 @@
 #include<iostream>
 
 int main(int argc, char * argv[]){
+    //std -> name space, hierarchical names
+	//:: separators, cout -> console out , endl-> end line
     std::cout <<"Insert a number : ";
     //scanf("%d", &a);
     int a;
-    std::cin>>a;
+    std::cin>>a; //cin -> console input, write in address of the integer a, the number writed in console
     int b = a*2;
     std::cout<< "The value is : " << b << std::endl;
     return 0;
-
 }
--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,13 @@
 /*
  * hello_world.cpp
  */
-#include <iostream>
+#include <iostream>// input output library
  
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[])//main function
+{	
+	// write in console out Hello Word!
+	//std -> name space, hierarchical names,
+	//:: separators, cout -> console out , endl-> end line
 	std::cout << "Hello world!" << std::endl;
 	return 0;
 }
- 
