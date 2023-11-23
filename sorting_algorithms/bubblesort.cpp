79ced43f7e7df9b9446832474d8e4634cae9189f
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-11-23 23:59:13 +0100

    bubblesort with template

---
+++ sorting_algorithms/bubblesort.cpp
@@ -0,0 +1,52 @@
+/*
+ author: Alessandro Ferrante
+ * bubblesort.cpp
+*/
+
+/*
+| The file introduces the Bubble Sort sorting algorithm with templates
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+#define N 20
+
+using namespace std;
+
+template <typename T> void _swap(T& a, T& b){
+    T tmp;
+
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>  void bubblesort(T * data, int n){
+    for (int i = 0;  i < n; i++){
+        for(int j = 0; i < n -1; j++){
+            if(data[j] > data[j+1])
+                // T temp = data[j];
+                // data[j] = data[j+1];
+                // data[j+1] = temp;
+                _swap<T>(data[j], data[j+1]);
+        }
+    }
+}
+
+int main(int argc, char ** argv ){
+
+    srand(time(NULL));
+    float data[N];
+    for(int i = 0; i< N; i++){
+        data[i] = ((float)rand()/ RAND_MAX * 50);
+    }
+
+    bubblesort<float>(data, N);
+    cout << "ciao";
+    for(int i = 0; i < N; i++){
+        cout << i << " " << data[i] << endl;
+    }
+    return 0;
+}
