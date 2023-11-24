894707aa66f1c5a9dc257624f27dc3ed4c8a495b
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-11-24 14:00:40 +0100

    bubblesort with template

--- sorting_algorithms/bubblesort.cpp
+++ sorting_algorithms/bubblesort.cpp
@@ -4,7 +4,10 @@
 */
 
 /*
-| The file introduces the Bubble Sort sorting algorithm with templates
+ | The file introduces the Bubble Sort sorting algorithm with templates
+ | The bubble sorting algorithm works by comparing adjacent elements 
+ | and swapping them if they are out of order. 
+ | This process is repeated until there are exchanges equal to the length of the array 
 */
 
 #include <iostream>
@@ -25,7 +28,7 @@ template <typename T> void _swap(T& a, T& b){
 
 template <typename T>  void bubblesort(T * data, int n){
     for (int i = 0;  i < n; i++){
-        for(int j = 0; i < n -1; j++){
+        for(int j = 0; j < n - 1; j++){
             if(data[j] > data[j+1])
                 // T temp = data[j];
                 // data[j] = data[j+1];
@@ -37,14 +40,12 @@ template <typename T>  void bubblesort(T * data, int n){
 
 int main(int argc, char ** argv ){
 
-    srand(time(NULL));
     float data[N];
     for(int i = 0; i< N; i++){
         data[i] = ((float)rand()/ RAND_MAX * 50);
     }
 
     bubblesort<float>(data, N);
-    cout << "ciao";
     for(int i = 0; i < N; i++){
         cout << i << " " << data[i] << endl;
     }
