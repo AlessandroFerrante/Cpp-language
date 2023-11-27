da4fb9bc9e314f4addaf81b5cb650b6c1d567d73
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-11-27 18:38:02 +0100

    quicksort with template

--- sorting_algorithms/quicksort.cpp
+++ sorting_algorithms/quicksort.cpp
@@ -10,6 +10,8 @@
  | In other words, the initial array is divided into smaller portions via partition, 
  | and each of these portions is then sorted separately by recursively applying the quicksort algorithm. 
  | This process continues until all portions are sorted, and the entire array is sorted.
+ _ The division of the array is virtual as is the combination of the individual 'subarrays', 
+ _ in fact the division and exchange operations take place on the array itself
  *
  | Partition function for the quicksort algorithm
  ? Parameters:
@@ -23,7 +25,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#define N 20
+#define N 10
 
 using namespace std;
 
@@ -31,7 +33,7 @@ template <typename T>  void _swap(T& a, T& b) {
     T tmp;
     tmp = a;
     a = b; 
-    b = a;
+    b = tmp;
 }
 
 template <typename T> int partition(T * data, int start, int end) {
@@ -54,7 +56,7 @@ template <typename T> int partition(T * data, int start, int end) {
     int left = start;
     int right = end;
 
-    // ? main loop to partition
+    // | main loop to partition
     while (left <= right) {
         // ? search for element on the left greater than the pivot, data[left] > pivot
         // ? if it is less than the pivot, move the index by increasing it
@@ -80,6 +82,10 @@ template <typename T> void quicksort(T * data, int start, int end) {
     if(start < end) {
         // ? to subdivide and 'sort' the array
         int split = partition<T>(data, start, end);
+        for (int i = 0;  i < N; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
         // ? recursive calls to sort the left and right subarrays
         quicksort<T>(data, start, split - 1);
         quicksort<T>(data, split + 1, end);
@@ -88,16 +94,24 @@ template <typename T> void quicksort(T * data, int start, int end) {
 
 int main(int argc, char ** argv){
     srand(time(NULL));
-    float data[N];
+    float data[] = {10, 8, 3, 15, 7, 9, 13, 4, 11, 6};
+    for (int i = 0;  i < N; i++) {
+        cout << data[i] << " ";
+    }
+    cout << endl;
+    /*
     for (int i = 0; i < N; i++) {
         data[i] = ((float)rand() / RAND_MAX * 50);
             cout << i << " " << data[i] <<endl;
     }
-
+    */
+    cout << "partitions" << endl;   
     quicksort<float>(data, 0, N -1);
-    
+    cout << endl;
     for (int i = 0;  i < N; i++) {
-        cout << i << " " << data[i] <<endl;
+        cout << data[i] << " ";
     }
+    cout << endl;
+
     return 0;
 }
