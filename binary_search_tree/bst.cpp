fa46c59c1e35afa72fe4db480efd76c003605364
Alessandro Ferrante <104135680+AlessandroFerrante@users.noreply.github.com>
2023-11-29 20:38:46 +0100

    simple bst

--- binary_search_tree/bst.cpp
+++ binary_search_tree/bst.cpp
@@ -5,27 +5,42 @@
 
 /*
  | The file introduces binary search trees with the use of struct.
-
- ? The bst_insert function 
-    - Scroll the bst based on the value to be inserted, 
-    - Following a path, searches for the correct position for the new node. 
-    - When it finds an empty location, it creates a new node with the specified value 
-    - and links it to the parent node on the left or right (or makes it root if the tree is empty).
- ? The bst_delete function
-    - The bst_delete function is designed to remove a specific node from a binary search tree (BST).
-    - It starts traversing the tree by looking for the node to delete and handles three main cases:
-    - the node to be eliminated is a leaf,
-    - has only one child (left or right),
-    - or has both children.
-    - In the case of only one child, it replaces the node to be deleted with its only child.
-    - In the case of both children, find the successor in order
-    - (the smallest node in the right subtree) to maintain the search property of the BST.
-    - The function manages the detachment of the node to be deleted and the reconfiguration of the pointers in the BST.
- ? The bst_find function
-    -
- ? The in_order function
-    -
- ? The 
+ *
+ - The bst_insert function 
+    ? Scroll the bst based on the value to be inserted, 
+    ? Following a path, searches for the correct position for the new node. 
+    ? When it finds an empty location, it creates a new node with the specified value 
+    ? and links it to the parent node on the left or right (or makes it root if the tree is empty).
+ *
+ - The bst_delete function
+    ? The bst_delete function is designed to remove a specific node from a binary search tree (BST).
+    ? It starts traversing the tree by looking for the node to delete and handles three main cases:
+    ? the node to be eliminated is a leaf,
+    ? has only one child (left or right),
+    ? or has both children.
+    ? In the case of only one child, it replaces the node to be deleted with its only child.
+    ? In the case of both children, find the successor in order
+    ? (the smallest node in the right subtree) to maintain the search property of the BST.
+    ? The function manages the detachment of the node to be deleted and the reconfiguration of the pointers in the BST.
+ *
+ - The visist_in_order function
+    ? In-order traversal visits the left subtree first, then the current node, and finally the right subtree. 
+    ? This approach follows the "left-node-right" order. 
+ *   
+ - The visist_in_pre_order function
+    ? Pre-order traversal prints the current node value before visiting the subtrees. 
+    ? The order is "node-left-right".
+ *
+ - The visist_in_post_order function
+    ? The post-order traversal first visits the subtrees (left and right) and then prints the value of the current node.
+    ? The order is "left-right-node".
+ *
+ - The bst_find function
+    ? Scroll through the tree to find the searched value
+ - The find_max function
+    ? Scrolls all the way down the right subtree to the last leaf to look for the maximum value
+ - The find_in function
+    ? Scrolls down the entire left subtree to the last leaf to look for the minimum value  
 */
 
 #include <iostream>
@@ -156,13 +171,73 @@ void bst_delete (tree root, int value) {
     }
 }
 
-void visit_in_order(tree root)
-{
+bool bst_find (tree root, int value) {
+    tree current = root;
+    // ? scroll the tree
+    while (current != nullptr){
+        // ? if the value of the node is equal to the one searched for, it returns true
+        if(current->value == value)
+            return true;
+        
+        // * se non ha trovato ancora il valore
+        // ? if the searched value is greater than the current node go to the right subtree
+        if(current->value < value)
+            current = current->right;
+        // ? if the searched value is less than the current node go to the left subtree
+        else
+            current = current->left;
+    }
+    // ? if it has finished scrolling the tree it has not found the value and returns false
+    return false;
+}
+
+// ? scroll through the entire left subtree
+const int find_max(tree current) {
+    while (current->right != nullptr)
+        current = current->right;
+    return current->value;
+}
+// ? scroll through the entire right subtree
+const int find_min(tree current) {
+    while (current->left != nullptr)
+        current = current->left;
+    return current->value;
+}
+
+// - left-node-right
+void visit_in_order(tree root) {
 	if (root != nullptr) {
+        // ? scrolls through the entire left subtree and then over the left children of the right subtree
 		visit_in_order(root->left);
+        // ? prints the current value, subtree first
 		cout << root->value << " ";
+        // ? scrolls to the right children of the left subtree and then to the right subtree
 		visit_in_order(root->right);
 	}
+}
+
+// - node-left-right
+void visit_in_pre_order(tree root){
+    if (root != nullptr) {
+        // ? prints the current value, node first
+        cout << root->value << " ";
+        // ? scrolls through the entire left subtree and then over the left children of the right subtree
+        visit_in_pre_order(root->left);
+        // ? scrolls to the right children of the left subtree and then to the right subtree
+        visit_in_pre_order(root->right);
+    }
+}
+
+// - right-left-node
+void visit_in_post_order(tree root) {
+    if (root != nullptr) {
+        // ? scrolls through the entire left subtree and then over the left children of the right subtree
+        visit_in_post_order(root->left);
+        // ? scrolls to the right children of the left subtree and then to the right subtree
+        visit_in_post_order(root->right);
+        // ? prints the current value, child right of subtree first
+        cout << root->value << " ";
+    }
 }
 
 int main(int agc, char ** argv){
@@ -187,7 +262,24 @@ int main(int agc, char ** argv){
 
     bst_delete(mybst, 35);
 
+    cout << "visit_in_order: " << endl;
     visit_in_order(mybst); cout << endl;
+    cout <<  "visit_in_pre_order" << endl;
+    visit_in_pre_order(mybst); cout << endl;
+    cout << "visit_in_post_order" << endl;
+    visit_in_post_order(mybst); cout << endl;
+    
+    cout << "Enter value to search for : ";
+    int v;
+    cin >> v;
+    
+    if(bst_find(mybst, v))
+        cout << "Valore trovato" << endl;
+    else
+        cout << "Valore non trovato" << endl;
+    
 
+    cout << "Max is: " << find_max(mybst) << endl;
+    cout << "Min is: " << find_min(mybst) << endl;
     return 0;
 }
